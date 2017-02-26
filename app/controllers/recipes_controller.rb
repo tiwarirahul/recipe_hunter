8c3d4c9b4984118617f998ada6273d0dc7c002ba
rahul <rahul.b.tiwari@accenture.com>
2017-02-26 16:44:19 +0530

    hardcoding env

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   def index
 
   	@search_term = params[:search]||'english'
-
+'byebug'
   	@recipes = Recipes.for(@search_term)
 
   	 end
--- app/models/recipes.rb
+++ app/models/recipes.rb
@@ -1,13 +1,13 @@
 class Recipes
 	 include HTTParty
 
-	 default_params key: ENV['FOOD2FORK_KEY']
+	 default_params key: '73e2ba517a8ba38f5e18fd91fbf4ad28'
 	 hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	 base_uri "http://#{hostport}/api"
 	 format :json
 
 	 def self.for term
-
+'byebug'
 	 	get("/search", query:{q: term})["recipes"]
 
 	 end
